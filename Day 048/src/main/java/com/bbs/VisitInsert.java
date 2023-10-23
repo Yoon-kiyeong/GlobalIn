99fbbf713468762a4a69564d914f062658d3bde0
Yoon-kiyeong <kyyoon2105@naver.com>
2023-10-23 16:56:51 +0900

    Day 048
    2023-10-23

--- Day 048/src/main/java/com/bbs/VisitInsert.java
+++ Day 048/src/main/java/com/bbs/VisitInsert.java
@@ -1,6 +1,9 @@
 package com.bbs;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
@@ -19,16 +22,48 @@ public class VisitInsert extends HttpServlet {
 		// 사용자가 요청한 값 읽기
 		String writer = request.getParameter("writer");
 		String memo = request.getParameter("memo");
-//		System.out.println("작성자 : " + writer);
-//		System.out.println("내용 : " + memo);
+		System.out.println("작성자 : " + writer);
+		System.out.println("내용 : " + memo);
+		
 		// 데이터베이스에 작성자와 내용을 저장하는 쿼리문 작성
+		StringBuffer sql = new StringBuffer();
+		sql.append("insert into visit(no, writer, memo, regdate) ");
+		sql.append("values(visit_seq.nextval,?,?,sysdate)");
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "scott";
+		String pass = "tiger";
+		// 데이터베이스 연결
+		Connection con = null;
+		PreparedStatement pstmt = null;
 		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(url, user, pass);
 			
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setString(1, writer);
+			pstmt.setString(2, memo);
+			pstmt.executeUpdate();
 		} catch(SQLException e) {
 			e.printStackTrace();
-		} finally {
-			
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
+		finally {
+			try {if(pstmt != null) 
+				pstmt.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		} try {
+			if(con != null) 
+				con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// 페이지 이동
+		response.sendRedirect("/bbs/VisitList");
 		
 	}
 	
