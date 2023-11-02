9f041cc5c6e39fd6a10385f9b561bd58217b463d
Yoon-kiyeong <kyyoon2105@naver.com>
2023-11-02 10:06:06 +0900

    Day 056
    2023-11-02

---
+++ Day 056/src/main/java/com/memberone/StudentDAO.java
@@ -0,0 +1,389 @@
+package com.memberone;
+
+import java.sql.*;
+import javax.sql.*;
+import javax.naming.*;
+import java.util.*;
+
+public class StudentDAO {
+	public static Connection getConnection() {
+		Connection con = null;
+
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
+			con = ds.getConnection();
+		} catch (Exception e) {
+			System.out.println("Connection 생성 실패");
+		}
+		return con;
+	}
+
+	// 입력받은 id값을 데이터베이스에 저장되어 있는 id값과 베교해서 중복이 되는지를 확인함
+	public boolean idCheck(String id) {
+
+		boolean result = true;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement("select * from student where id=?");
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+
+			if (!rs.next()) {
+				result = false;
+			}
+		} catch (SQLException s) {
+			s.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException ss) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException ss) {
+				}
+			}
+
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ss) {
+				}
+			}
+		}
+		return result;
+	} // end idCheck
+
+	// 우편번호를 데이터베이스에서 검색해서 Vector에 저장해서 리턴해주는 메소드 구현
+	public Vector<ZipcodeVO> zipcodeRead(String dong) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Vector<ZipcodeVO> vecList = new Vector<ZipcodeVO>();
+
+		try {
+			con = getConnection();
+
+			String sql = "select * from zipcode where dong like '%" + dong + "%'";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				ZipcodeVO tempZipcode = new ZipcodeVO();
+				tempZipcode.setZipcode(rs.getString("zipcode"));
+				tempZipcode.setSido(rs.getString("sido"));
+				tempZipcode.setGugun(rs.getString("gugun"));
+				tempZipcode.setDong(rs.getString("dong"));
+				tempZipcode.setRi(rs.getString("ri"));
+				tempZipcode.setBunji(rs.getString("bunji"));
+				vecList.addElement(tempZipcode);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+				}
+		}
+		return vecList;
+	}
+
+	// 회원가입 폼에서 회원가입 버튼을 누르면 실제 데이터베이스로 회원정보가 저장될 수 있는 기능을 구현
+	public boolean memberInsert(StudentVO vo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		boolean flag = false;
+
+		try {
+			con = getConnection();
+			String sql = "insert into student values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getId());
+			pstmt.setString(2, vo.getPass());
+			pstmt.setString(3, vo.getName());
+			pstmt.setString(4, vo.getPhone1());
+			pstmt.setString(5, vo.getPhone2());
+			pstmt.setString(6, vo.getPhone3());
+			pstmt.setString(7, vo.getEmail());
+			pstmt.setString(8, vo.getZipcode());
+			pstmt.setString(9, vo.getAddress1());
+			pstmt.setString(10, vo.getAddress2());
+
+			int count = pstmt.executeUpdate();
+			if (count > 0)
+				flag = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+				}
+		}
+		return flag;
+	} // end memberInsert
+
+	/*
+	 * 로그인 버튼을 클릭하면 데이터베이스에 저장된 id/pass와 입력된 데이터와 비교하여 같으면 성공, 다르면 로그인 실패처리를 하는 메소드
+	 * 구현 정보를 비교하여 그 결과를 정수형으로 리턴 1: 로그인 성공, 0: 비밀번호 오류, -1: 아이디가 존재하지 않음
+	 */
+	public int loginCheck(String id, String pass) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int check = -1;
+
+		try {
+			con = getConnection();
+
+			// DB 조회문
+			String sql = "select pass from student where id = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				String dbPass = rs.getString("pass");
+
+				// 사용자가 입력한 비밀번호와 데이터베이스에 저장된 비밃번호를 비교
+				if (pass.equals(dbPass))
+					check = 1;
+				else
+					check = 0;
+			}
+		} catch (SQLException s) {
+			s.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
+		return check;
+	}
+
+	/*
+	 * 정보수정 보튼을 클릭하면 현재 로그인한 회원의 정보를 수정할 수 있도록 화면에 보여줘야 함
+	 * 
+	 * 세션에 저장된 아이디로 회원정보를 가져오는 메소드 구현
+	 * 
+	 */
+
+	public StudentVO getMember(String id) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StudentVO vo = null;
+
+		try {
+			con = getConnection();
+			String sql = "select * from student where id = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) { // 아이디가 회원정보가 존재한다면
+				vo = new StudentVO();
+				vo.setId(rs.getString("id"));
+				vo.setPass(rs.getString("pass"));
+				vo.setName(rs.getString("name"));
+				vo.setPhone1(rs.getString("phone1"));
+				vo.setPhone2(rs.getString("phone2"));
+				vo.setPhone3(rs.getString("phone3"));
+				vo.setEmail(rs.getString("email"));
+				vo.setZipcode(rs.getString("zipcode"));
+				vo.setAddress1(rs.getString("address1"));
+				vo.setAddress2(rs.getString("address2"));
+			}
+		} catch (Exception e) {
+
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
+
+		return vo;
+	}
+
+	/*
+	 * 정보수정 버튼을 클릭하면 데이터베이스에 정보가 수정되어 저장된다.
+	 * 
+	 */
+	public void updateMemeber(StudentVO vo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = getConnection();
+			String sql = "update student set pass=?, phone1=?, phone2=?, phone3=?, email=?, zipcode=?, address1=?, address2=? where id=?";
+			// 수정 쿼리문 작성
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getPass());
+			pstmt.setString(2, vo.getPhone1());
+			pstmt.setString(3, vo.getPhone2());
+			pstmt.setString(4, vo.getPhone3());
+			pstmt.setString(5, vo.getEmail());
+			pstmt.setString(6, vo.getZipcode());
+			pstmt.setString(7, vo.getAddress1());
+			pstmt.setString(8, vo.getAddress2());
+			pstmt.setString(9, vo.getId());
+
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
+	} // end updateMember
+
+	/*
+	 * 회원 탈퇴 회원의 비밀번호를 입력받아서 데이터베이스의 비밀번호와 일치하는지를 먼저 비교해야 함 만약 일치한다면 회원탈퇴 처리하고, 그렇지
+	 * 않으면 비밀번호가 틀렸다고 알려주어야 함
+	 */
+
+	public int deleteMember(String id, String pass) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String dbPass = "";
+		int result = -1; // 회원정보 등록
+
+		try {
+			con = getConnection();
+			String sql = "select pass from student where id = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+
+				dbPass = rs.getString("pass");
+
+				if (dbPass.equals(pass)) { // 비밀번호가 일치하면
+					pstmt = con.prepareStatement("delete from student where id = ?");
+
+					pstmt.setString(1, id);
+					pstmt.executeUpdate();
+					result = 1; // 회원탈퇴 성공
+				} else { // 비밀번호가 일치하지 않을 경우
+					result = 0;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		return result;
+	}
+}
---
+++ Day 056/src/main/webapp/boardone/list.jsp
@@ -0,0 +1,184 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="com.boardone.BoardDAO"%>
+<%@ page import="com.boardone.BoardVO"%>
+<%@ page import="java.util.List"%>
+<%@ page import="java.text.SimpleDateFormat"%>
+<%@ include file="view/color.jsp"%>
+<%!//1. 한 페이지에 보여줄 목록 수 지정
+	int pageSize = 4;
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");%>
+
+<%
+//2. 파라미터 받아옴
+String pageNum = request.getParameter("pageNum");
+String searchWhat = request.getParameter("searchWhat");
+// 무엇을 검색할지를 받아옴(작성자, 제목, 내용 중 하나)
+
+String searchText = request.getParameter("searchText");
+// 검색할 내용
+
+// 폼에서 가져온 파라미터 값을 한글로 변환처리 함
+if (searchText != null) {
+	searchText = new String(searchText.getBytes("utf-8"), "utf-8");
+}
+
+if (pageNum == null) {
+	pageNum = "1";
+}
+
+int currentPage = Integer.parseInt(pageNum);
+int startRow = (currentPage - 1) * pageSize + 1;
+int endRow = currentPage * pageSize;
+
+int count = 0;
+int number = 0;
+List<BoardVO> articleList = null;
+
+BoardDAO dbPro = BoardDAO.getInstance();
+
+// 검색이 아니면 전체 목록을 보여주고 검색이면 검색한 내용을 보여줌
+if (searchText == null) {
+	count = dbPro.getArticleCount();
+	if (count > 0) {
+		articleList = dbPro.getArticle(startRow, endRow); // 3;
+	}
+} else { // 검색일 경우
+	count = dbPro.getArticleCount(searchWhat, searchText);
+	if (count > 0) {
+		articleList = dbPro.getArticle(startRow, endRow, searchWhat, searchText); // 3;
+	}
+}
+count = dbPro.getArticleCount();
+
+if (count > 0) {
+	articleList = dbPro.getArticle(startRow, endRow);
+}
+number = count - (currentPage - 1) * pageSize;
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>게시글 목록</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body bgcolor="<%=bodyback_c%>">
+	<div align="center">
+		<b>글 목록(전체 글 : <%=count%>)
+		</b>
+		<table width="700">
+			<tr>
+				<td align="right" bgcolor="<%=value_c%>"><a
+					href="writeForm.jsp">글쓰기</a></td>
+			</tr>
+		</table>
+
+		<%
+		if (count == 0) {
+		%>
+		<table width="700" border="1" cellpadding="1" cellspacing="0">
+			<tr>
+				<td>게시판에 저장된 글이 없습니다</td>
+			</tr>
+		</table>
+		<%
+		} else {
+		%>
+		<table width="700" border="1" cellpadding="1" cellspacing="0"
+			align="center">
+			<tr height="30" bgcolor="<%=value_c%>">
+				<td align="center" width="50">번호</td>
+				<td align="center" width="250">제목</td>
+				<td align="center" width="150">작성자</td>
+				<td align="center" width="150">작성일</td>
+				<td align="center" width="50">조회</td>
+				<td align="center" width="100">IP</td>
+			</tr>
+
+			<%
+			for (int i = 0; i < articleList.size(); i++) {
+				BoardVO article = (BoardVO) articleList.get(i);
+			%>
+			<tr height="30">
+				<td width="50" align="center"><%=number--%></td>
+				<td width="250">
+					<%
+					int wid = 0;
+
+					if (article.getDepth() > 0) {
+						wid = 5 * (article.getDepth());
+					%> <img src="img/level.gif" width="<%=wid%>" height="16"> <img
+					src="img/re.gif"> <%
+ } else {
+ %> <img src="img/level.gif" width="<%=wid%>" height="16"> <%
+ }
+ %> <a href="content.jsp?num=<%=article.getNum()%>&pageNum=1"> <%=article.getSubject()%></a>
+
+					<%
+					if (article.getReadcount() >= 5) {
+					%> <img src="img/hot.gif" border="0" height="16"> <%
+ }
+ %>
+				</td>
+				<td width="150" align="center"><a
+					href="mailto:<%=article.getEmail()%>"> <%=article.getWriter()%></a>
+				</td>
+				<td width="150" align="center"><%=sdf.format(article.getRegdate())%>
+				</td>
+				<td width="50" align="center"><%=article.getReadcount()%></td>
+				<td width="100" align="center"><%=article.getIp()%></td>
+			</tr>
+			<%
+			}
+			%>
+		</table>
+		<%
+		}
+		%>
+		<!-- 페이징 처리 -->
+		<%
+		if (count > 0) {
+			int pageBlock = 3;
+			int imsi = count % pageSize == 0 ? 0 : 1;
+			int pageCount = count / pageSize + imsi;
+
+			int startPage = (int) ((currentPage - 1) / pageBlock) * pageBlock + 1;
+			int endPage = startPage + pageBlock - 1;
+
+			if (endPage > pageCount)
+				endPage = pageCount;
+
+			if (startPage > pageBlock) {
+		%>
+		<a href="list.jsp?pageNum=<%=startPage - pageBlock%>">[이전]</a>
+		<%
+		}
+
+		for (int i = startPage; i <= endPage; i++) {
+		%>
+		<a href="list.jsp?pageNum=<%=i%>">[<%=i%>]
+		</a>
+		<%
+		}
+
+		if (endPage < pageCount) {
+		%>
+		<a href="list.jsp?pageNum=<%=startPage + pageBlock%>">[다음]</a>
+		<%
+		}
+
+		}
+		%>
+
+		<form action="list.jsp">
+			<select name="searchWhat">
+				<option value="writer">제목</option>
+				<option value="subject">작성자</option>
+				<option value="content">내용</option>
+			</select> <input type="text" name="searchText"> <input type="submit"
+				value="검색">
+		</form>
+	</div>
+</body>
+</html>
